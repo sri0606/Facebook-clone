e832dc7c7ed55cab4843ca67e1ebf0eb635b1b50
sri0606 <srirams0606@gmail.com>
2024-01-02 02:04:12 -0500

    removed unnecessary tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,52 +4,42 @@ import { Pressable, useColorScheme } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
-export default function TabLayout() {
+const TabLayout=()=> {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors.primary,
       }}>
       <Tabs.Screen
         name="index"
         options={{ 
-          tabBarLabel: 'Home',
+          headerShown: false,
+          tabBarLabel: "Home",
           tabBarIcon: ({ color }) => <FontAwesome name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="friends"
-        options={{
+        options={{ 
+          headerShown: false,
           tabBarLabel: 'Friends',
           tabBarIcon: ({ color }) => <Ionicons name="people-outline" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="video"
-        options={{
-          tabBarLabel: 'Video',
-          tabBarIcon: ({ color }) => <Ionicons name="play-circle-outline" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="marketplace"
-        options={{
-          tabBarLabel: 'Marketplace',
-          tabBarIcon: ({ color }) => <FontAwesome name="shopping-cart" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="notifications"
-        options={{
+        options={{ 
+          headerShown: false,
           tabBarLabel: 'Notifications',
           tabBarIcon: ({ color }) => <Ionicons name="notifications-outline" color={color} />,
         }}
       />
       <Tabs.Screen
         name="menu"
-        options={{
+        options={{ 
+          headerShown: false,
           tabBarLabel: 'Menu',
           tabBarIcon: ({ color }) => <Ionicons name="menu-outline" color={color} />,
         }}
@@ -57,3 +47,6 @@ export default function TabLayout() {
     </Tabs>
   );
 }
+
+
+export default TabLayout;
